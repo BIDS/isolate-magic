a18377da976e5d2c5fc1ee67c0fae2d059246a62
Yu Feng <rainwoodman@gmail.com>
2014-12-16 22:33:40 -0800

    support nested cell magics

--- 50-isolate-magic.py
+++ 50-isolate-magic.py
@@ -1,22 +1,55 @@
-# put this file in .ipython/profile_[xxxxxx]/startup
-
 __all__ = []
 from IPython.core.magic import (Magics, magics_class, line_magic,
                                         cell_magic, line_cell_magic)
 import re
+
+class PreConditionError(NameError):
+    pass
+class PostConditionError(NameError):
+    pass
+
 @magics_class
 class IsolateMagics(Magics):
     @cell_magic('isolate')
-    def isolate(self, line, cell):
+    def isolate(self, line, cell, protected=True):
         pre, post = self.parser(line)
-        print 'pre', pre
-        print 'post', post
-        ns = self.shell.user_ns.copy()
+
+        oldnames = set(self.shell.user_ns.keys())
+        badnames = []
+
         for symbol in pre:
-            assert symbol in ns
-        exec (cell, ns)
+            if symbol.startswith('@'):
+                continue
+            if not symbol in oldnames:
+                badnames.append(symbol)
+        if len(badnames) > 0:
+            raise PreConditionError("The following variables are undefined: %s",
+                    str(badnames))
+
+        # the juice is here
+        # ipython processes other magics
+        self.shell.run_cell(cell)
+
+        badnames = []
+
         for symbol in post:
-            self.shell.user_ns[symbol] = ns[symbol]
+            if symbol.startswith('@'):
+                continue
+            if not symbol in self.shell.user_ns:
+                badnames.append(symbol)
+
+        # alright, due to #7256 we can't tell if run_cell failed or not
+        # so on failures we get a second error due to post-conditions.
+        if len(badnames) > 0:
+            raise PostConditionError("The following variables are undefined: %s",
+                    str(badnames))
+
+        if protected:
+            # now remove extra symbols in the user ns:
+            for symbol in self.shell.user_ns.keys():
+                if symbol not in post and symbol not in oldnames:
+                    self.shell.user_ns.pop(symbol)
+
 
     def parser(self, line):
         clauses = re.findall('(\s*(pre|post)\(([^)]*)\))', line)
@@ -24,12 +57,11 @@ class IsolateMagics(Magics):
         post = []
         for c in clauses:
             if c[1].lower() == 'pre':
-                pre = c[2].split(',')
+                pre = [a.strip() for a in c[2].split(', ')]
             elif c[1].lower() == 'post':
-                post = c[2].split(',')
+                post = [a.strip() for a in c[2].split(', ')]
         return pre, post
 
-        eval(cell, locals)
 
 # In order to actually use these magics, you must register them with a
 # running IPython.  This code must be placed in a file that is loaded
